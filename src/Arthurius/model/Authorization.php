042acd91da19ecf2fb06ab201ce93412ba8c9d06
Soldiertt <soldiertt@gmail.com>
2017-11-11 11:25:11 +0100

    fix authorization secrets

--- src/Arthurius/model/Authorization.php
+++ src/Arthurius/model/Authorization.php
@@ -7,17 +7,14 @@
  */
 
 namespace Arthurius\model;
-
+use Arthurius\secrets\Secrets;
 
 class Authorization
 {
     public static function checkIsAdmin($request) {
         $headerUserId = $request->getHeader('X-Arth-UserId')[0];
         if ($headerUserId != null) {
-            return $headerUserId == 'google-oauth2|116562219924228798186' ||
-                $headerUserId == 'auth0|5920c0dc549d1f23d38b08d3' ||
-                $headerUserId == 'auth0|58bc2dc8a0ba8d24312f907d' ||
-                $headerUserId == 'google-oauth2|111406302913101904579';
+            return in_array($headerUserId, Secrets::$ADMIN_USER_KEYS);
         }
         return false;
     }
